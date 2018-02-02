8286950ae110dde209894715c24f481c54686fda
Robb Shecter <robb@public.law>
2018-02-02 01:10:08 -0800

    Docs

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Adapted from Alex Kleissner's post, Running a Phoenix 1.3 project with docker-compose
+# https://medium.com/@hex337/running-a-phoenix-1-3-project-with-docker-compose-d82ab55e43cf
+
 set -e
 
 
